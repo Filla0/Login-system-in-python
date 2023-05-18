148cd10544ebd34eac328874f14b7ee6d18ba991
Filimon Haftu <filimonhaftu18@gmail.com>
2023-05-18 12:35:18 +0200

    added my email and pass

--- GFG/gfg/info.py
+++ GFG/gfg/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'gfg.demo.django.login@gmail.com'
-EMAIL_HOST_PASSWORD= 'password'
+EMAIL_HOST_USER = 'dlogin606@gmail.com'
+EMAIL_HOST_PASSWORD= 'Filimon@1'
 EMAIL_PORT = 587
