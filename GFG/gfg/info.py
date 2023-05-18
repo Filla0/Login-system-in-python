e97874ae7114e4dfbcfc353dceb90603ed407445
Filimon Haftu <filimonhaftu18@gmail.com>
2023-05-18 23:08:15 +0200

    seding confirmation email

--- GFG/gfg/info.py
+++ GFG/gfg/info.py
@@ -1,5 +1,6 @@
 EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'dlogin606@gmail.com'
-EMAIL_HOST_PASSWORD= 'Filimon@1'
+EMAIL_HOST_PASSWORD= 'ymxujddlzemdbfuw'
 EMAIL_PORT = 587
